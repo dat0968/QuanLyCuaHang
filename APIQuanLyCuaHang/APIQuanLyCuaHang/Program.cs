d56a812d07c925b884e4b7a7018734b920794896
Trực Trần Trung <tructtpk03625@gmail.com>
2025-03-27 21:56:59 +0700

    feat(api.comment): add config to show comment for action api

--- APIQuanLyCuaHang/APIQuanLyCuaHang/Program.cs
+++ APIQuanLyCuaHang/APIQuanLyCuaHang/Program.cs
@@ -1,10 +1,12 @@
-﻿using APIQuanLyCuaHang.DbInitializer;
+﻿using System.Reflection;
+using APIQuanLyCuaHang.DbInitializer;
 using APIQuanLyCuaHang.Models;
 using APIQuanLyCuaHang.Repositories.CaKip;
 using APIQuanLyCuaHang.Repositories.Dashboard;
 using APIQuanLyCuaHang.Repositories.LichLamViec;
 using APIQuanLyCuaHang.Repositories.UnitOfWork;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +15,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+#region Đăng ký Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    #region Format thêm comment lên môi action
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+    #endregion
+
+});
+#endregion
 builder.Services.AddDbContext<QuanLyCuaHangContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("QuanLyCuaHangContext"));
+options.UseSqlServer(builder.Configuration.GetConnectionString("QuanLyCuaHangContext"));
 });
 
 builder.Services.AddScoped<IDashboardRepository, DashboardRepository>();
