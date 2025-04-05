4c20a127127dd0f8d356796bb3ff59e52429ef6b
Trực Trần Trung <tructtpk03625@gmail.com>
2025-04-05 10:48:13 +0700

    ui(link): add link navigation to client-order

---
+++ QuanLyCuaHangUXUI/QuanLyCuaHangProject/src/components/ui/OrderClientButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterLink class="text-link-format text-dark" to="/client-order" title="Đơn hàng khách hàng">
+    <i class="icon-handbag"></i>
+  </RouterLink>
+</template>
+
+<script>
+export default {
+  name: 'OrderClientButton',
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style scoped></style>
