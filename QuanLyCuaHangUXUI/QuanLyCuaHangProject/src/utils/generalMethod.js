a784892e77ba3d27eebfe3ad7c57edb8a21497d0
Trực Trần Trung <tructtpk03625@gmail.com>
2025-04-12 20:59:17 +0700

    feat(img): add image processing method in generalMethod.js

---
+++ QuanLyCuaHangUXUI/QuanLyCuaHangProject/src/utils/generalMethod.js
@@ -0,0 +1,13 @@
+export function getImageUrl(imageName, pathFolder = '/') {
+  const baseUrl = 'https://localhost:7139'
+  // Kiểm tra xem imageName có phải là một URL đầy đủ hay không
+  const isUrl = /^https?:\/\//i.test(imageName)
+
+  if (isUrl) {
+    // Nếu là URL, chỉ cần trả về imageName
+    return imageName
+  } else {
+    // Nếu chỉ là tên hình ảnh, ghép với address
+    return `${baseUrl}${pathFolder}${imageName}`
+  }
+}
